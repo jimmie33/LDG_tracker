b4d398e59adaed0c4df7d6eb93f2ac9e4f8ee4cd
jimmie33 <jimmie33@gmail.com>
2013-07-19 21:26:13 -0400

    improve the speed for getFeatureRep ad sampling+testing

--- tracker/updateSample.m
+++ tracker/updateSample.m
@@ -16,24 +16,31 @@ rad = (rand(size(temp,1)-1,1))*(radius*max([sampler.template_width,sampler.templ
 angle = rand(size(temp,1)-1,1)*2*pi;
 temp(2:end,1:2) = temp(2:end,1:2) + [cos(angle).*rad,sin(angle).*rad];
 
-sampler.patterns_dt=zeros(sample_sz,size(sampler.template,1)*size(sampler.template,2)*size(I_vf,3));
-valid_sample = boolean(zeros(1,sample_sz));
 
-for i=1:sample_sz
-   rect=temp(i,:);
-%    upleft = round([rect(1)-sampler.roi(1)+1,rect(2)-sampler.roi(2)+1]);
-   if ~((rect(1)<1) || (rect(2)<1) || (rect(1)+rect(3)>size(I_vf,2)) || (rect(2)+rect(4)>size(I_vf,1)))
-       sub_win=I_vf(round(rect(2): sampler.step:(rect(2)+rect(4))),round(rect(1): sampler.step : (rect(1)+rect(3))),:);
-       sampler.patterns_dt(i,:) = sub_win(:)';
-       valid_sample(i) = 1;%valid sample
-   end
+% valid_sample = boolean(zeros(1,sample_sz));
+valid_sample = ~(temp(:,1)<1 | temp(:,2)<1 | temp(:,1)+temp(:,3)>size(I_vf,2) | temp(:,2)+temp(:,4)>size(I_vf,1));
+temp = temp(valid_sample,:);
+sampler.state_dt = temp;
+sampler.state_dt(:,1) = sampler.state_dt(:,1)+sampler.roi(1)-1;
+sampler.state_dt(:,2) = sampler.state_dt(:,2)+sampler.roi(2)-1;
+
+% sampler.patterns_dt=zeros(size(sampler.state_dt,1),size(sampler.template,1)*size(sampler.template,2)*size(I_vf,3));
+max_confidence = -inf;
+for i=1:size(sampler.state_dt,1)
+    rect=temp(i,:);
+    sub_win=I_vf(round(rect(2): sampler.step:(rect(2)+rect(4))),round(rect(1): sampler.step : (rect(1)+rect(3))),:);
+    confidence = -(sub_win(:)'*svm_tracker.w'+svm_tracker.Bias);
+    if confidence > max_confidence
+        max_confidence = confidence;
+        svm_tracker.output = sampler.state_dt(i,:);
+        svm_tracker.confidence = confidence;
+    end
+%     sampler.patterns_dt(i,:) = sub_win(:)';
 end
 
 %% exclude invalide samples
-sampler.patterns_dt = sampler.patterns_dt(valid_sample,:);
-sampler.state_dt = temp(valid_sample,:);
-sampler.state_dt(:,1) = sampler.state_dt(:,1)+sampler.roi(1)-1;
-sampler.state_dt(:,2) = sampler.state_dt(:,2)+sampler.roi(2)-1;
+% sampler.patterns_dt = sampler.patterns_dt(valid_sample,:);
+
 
 end
 
